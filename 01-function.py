16fa13ec485b538eef3e65142ad487a3fab3349f
Mike. Shi <ashida2016@outlook.com>
2019-06-30 23:23:50 +0800

    updated

--- 01-function.py
+++ 01-function.py
@@ -7,6 +7,7 @@
 # 掌握函数的用法
 
 # 练习一：第一个函数
+print('\n---华丽分割线---练习（1）---')
 def hello():
     print("Function-Hello World!")
     return
@@ -15,6 +16,7 @@ def hello():
 hello()
 
 # 练习二：打印指定字符串 - 传入字符串
+print('\n---华丽分割线---练习（2）---')
 def printme( str ):
    # 打印任何传入的字符串
    print (str)
@@ -24,6 +26,7 @@ def printme( str ):
 printme("xxxxx")
 
 # 练习三：计算面积函数 - 传入数字
+print('\n---华丽分割线---练习（3）---')
 def area(width, height):
     return width * height
 
@@ -32,8 +35,8 @@ w = 4
 h = 5
 print("width =", w, " height =", h, " area =", area(w, h))
 
-
 # 练习四：将求三角形面积的海伦公式函数化
+print('\n---华丽分割线---练习（4）---')
 def triangle_area(a, b, c):
     # 依据海伦公式求任意三角形面积
     # 已知三角形三边a,b,c,半周长p,则S＝ √[p(p - a)(p - b)(p - c)] （海伦公式）（p=(a+b+c)/2）
